6724dad45fd10d31a8c2e6d3ca13b5ed791ac4ce
T.Ingaran <mringaran1997.com>
2025-02-05 11:45:51 +0530

    update_1

--- myapp/settings.py
+++ myapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a67l*1_jxmoot&=w@5cd#b$1_0oa&bb$d$snfxsh^j=%(7q_ap
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
